a6d0542df1cc7534a997a49b1185262644d6f761
jai <jai@pearlorganisation.com>
2024-11-27 11:33:39 +0530

    updates

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,6 +7,7 @@ import { Plans, PlansSchema } from 'src/schemas/Plans.schema';
 import { SubscriptionModule } from 'src/subscription/subscription.module';
 import { BillingHistoryModule } from 'src/billing-history/billing-history.module';
 import { AuthSuperAdminMiddleware } from 'src/middlewares/authSuperAdmin.Middleware';
+import { AuthAdminTokenMiddleware } from 'src/middlewares/authAdmin.Middleware';
 
 @Module({
   imports: [
@@ -29,6 +30,11 @@ import { AuthSuperAdminMiddleware } from 'src/middlewares/authSuperAdmin.Middlew
 })
 export class UsersModule {
   configure(consumer: MiddlewareConsumer) {
+
+    consumer
+    .apply(AuthAdminTokenMiddleware)
+    .forRoutes({ path: 'users/employees/*', method: RequestMethod.ALL });
+
     consumer
       .apply(AuthSuperAdminMiddleware)
       .forRoutes({ path: 'users/clients/*', method: RequestMethod.ALL });
