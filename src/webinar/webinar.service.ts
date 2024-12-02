588a7341ac5eb308e2539d50bce5f85059f55dd4
jai <jai@pearlorganisation.com>
2024-12-02 16:57:33 +0530

    updates

--- src/webinar/webinar.service.ts
+++ src/webinar/webinar.service.ts
@@ -1,31 +1,69 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Webinar } from 'src/schemas/Webinar.schema';
-import { CreateWebinarDto } from './dto/createWebinar.dto';
+import { CreateWebinarDto, UpdateWebinarDto } from './dto/createWebinar.dto';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class WebinarService {
-    constructor(
-        @InjectModel(Webinar.name) private webinarModel: Model<Webinar>,
-        private readonly configService: ConfigService,
-    ){}
+  constructor(
+    @InjectModel(Webinar.name) private webinarModel: Model<Webinar>,
+    private readonly configService: ConfigService,
+  ) {}
 
-    async createWebiar(createWebinarDto: CreateWebinarDto):Promise<any> {
-        //create webinar
+  async createWebiar(createWebinarDto: CreateWebinarDto): Promise<any> {
+    //create webinar
+    console.log(createWebinarDto)
+    const result = await this.webinarModel.create(createWebinarDto);
+    return result;
+  }
 
-    }
+  async getWebinars(adminId: string, page, limit): Promise<any> {
+    const skip = (page - 1) * limit;
+    console.log(adminId);
+    //get all webinars for adminas per user id
+    const result = await this.webinarModel
+      .find({ adminId: new Types.ObjectId(`${adminId}`) })
+      .sort({ createdAt: 1 })
+      .skip(skip)
+      .limit(limit);
+    return result;
+  }
 
-    async getWebinars(userId: string):Promise<any> {
-        //get all webinars for adminas per user id
-    }
+  async getWebinar(id: string, adminId: string): Promise<any> {
+    //get all webinars as per user id
 
-    async getWebinar(id: string, userId: string):Promise<any> {
-        //get all webinars as per user id
-    }
+    const result = await this.webinarModel.find({
+      _id: new Types.ObjectId(`${id}`),
+      adminId: adminId,
+    });
+    return result;
+  }
 
-    async updateWebinar():Promise<any> {
-        //update webinar
-    }
+  async updateWebinar(
+    id: string,
+    adminId: string,
+    updateWebinarDto: UpdateWebinarDto,
+  ): Promise<any> {
+    //update webinar
+    console.log(id, adminId);
+    const result = await this.webinarModel.findOneAndUpdate(
+      {
+        _id: new Types.ObjectId(`${id}`),
+        adminId: new Types.ObjectId(`${adminId}`),
+      },
+      updateWebinarDto,
+    );
+    return result;
+  }
+
+  async deleteWebinar(id: string, adminId: string): Promise<any> {
+    //update webinar
+    const result = await this.webinarModel.findOneAndDelete({
+      _id: new Types.ObjectId(`${id}`),
+      adminId: adminId,
+    });
+    return result;
+  }
 }
