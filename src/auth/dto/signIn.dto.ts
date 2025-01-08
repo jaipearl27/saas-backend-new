b804cfb96e85f586ed3eab9678e6414219787331
jai <jai@pearlorganisation.com>
2025-01-08 18:21:16 +0530

    updatse

--- src/auth/dto/signIn.dto.ts
+++ src/auth/dto/signIn.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from "class-validator";
 export class SignInDto {
     @IsString()
     @IsNotEmpty()
-    userName: string
+    email: string
 
     @IsString()
     @IsNotEmpty()
